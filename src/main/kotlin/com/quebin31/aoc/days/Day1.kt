cd6a1cd2a918b0a3fdbf9bb9712fcea3f8ad6f6f
Kevin <quebin31@gmail.com>
2022-12-01 10:22:45 -0500

    feat: Solve day 1 (with extra improvements after initial solution)

--- src/main/kotlin/com/quebin31/aoc/days/Day1.kt
+++ src/main/kotlin/com/quebin31/aoc/days/Day1.kt
@@ -1,14 +1,56 @@
 package com.quebin31.aoc.days
 
 import com.quebin31.aoc.core.Day
+import com.quebin31.aoc.utils.chunkedBy
+import com.quebin31.aoc.utils.sortedDescending
+import java.util.*
 
+@Suppress("unused")
 class Day1 : Day {
 
-    override fun part1(input: Sequence<String>): Any {
-        TODO("Not yet implemented")
+    override fun part1(input: Sequence<String>): Any =
+        maxCaloriesImperative(input, n = 1).single()
+
+    override fun part2(input: Sequence<String>): Any =
+        maxCaloriesImperative(input, n = 3).sum()
+
+    /**
+     * Elegant. More memory footprint as it uses two hidden stateful [Sequence] operations, one in
+     * [chunkedBy] and another one in [sortedDescending].
+     */
+    private fun maxCaloriesFunctional(input: Sequence<String>, n: Int): List<Int> = input
+        .chunkedBy { it.isBlank() }
+        .map { inventory -> inventory.sumOf { it.toInt() } }
+        .sortedDescending(n)
+        .toList()
+
+    /**
+     * Has the benefit of using a single for loop with less memory footprint as the sum of
+     * calories is kept temporarily in a single variable.
+     */
+    private fun maxCaloriesImperative(input: Sequence<String>, n: Int): List<Int> {
+        val maxCarriedCalories = sortedSetOf<Int>(reverseOrder())
+        var currentSum = 0
+
+        for (line in input) {
+            if (line.isBlank()) {
+                maxCarriedCalories.update(currentSum, n)
+                currentSum = 0
+            } else {
+                currentSum += line.toInt()
+            }
+        }
+
+        return maxCarriedCalories.toList()
     }
 
-    override fun part2(input: Sequence<String>): Any {
-        TODO("Not yet implemented")
+    /**
+     * Updates the [TreeSet] adding a new value, and removing the lowest if the size would exceed [n].
+     */
+    private fun TreeSet<Int>.update(value: Int, n: Int) {
+        add(value)
+        if (size > n) {
+            pollLast()
+        }
     }
 }
