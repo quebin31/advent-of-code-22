e8eb1435c5fff1c7b1ba7a76323d6b1da5754405
Kevin <quebin31@gmail.com>
2022-12-04 08:42:38 -0500

    feat: Solve day 4

---
+++ src/main/kotlin/com/quebin31/aoc/days/Day4.kt
@@ -0,0 +1,30 @@
+package com.quebin31.aoc.days
+
+import com.quebin31.aoc.core.Day
+
+
+@Suppress("unused")
+class Day4 : Day {
+
+    override fun part1(input: Sequence<String>): Any = parseInput(input).count { (a, b) ->
+        a.contains(b) || b.contains(a)
+    }
+
+    override fun part2(input: Sequence<String>): Any = parseInput(input).count { (a, b) ->
+        a.overlaps(b) || b.overlaps(a)
+    }
+
+    private fun parseInput(input: Sequence<String>) = input
+        .map { line ->
+            val (a, b) = line.split(",")
+            val (aMin, aMax) = a.split("-").map { it.toInt() }
+            val (bMin, bMax) = b.split("-").map { it.toInt() }
+            (aMin..aMax) to (bMin..bMax)
+        }
+
+    private fun IntRange.contains(other: IntRange): Boolean =
+        other.first >= first && other.last <= last
+
+    private fun IntRange.overlaps(other: IntRange): Boolean =
+        first in other || last in other
+}
